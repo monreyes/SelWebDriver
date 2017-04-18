03b88645e70d4d1160d5ab4be34be76f4bcaf0b3
monreyes <ramon_g_reyes@yahoo.com>
2017-04-17 22:09:27 -0500

    updating control structures

--- PythonProj1/controlstructure/boolean-precedence.py
+++ PythonProj1/controlstructure/boolean-precedence.py
@@ -4,10 +4,26 @@
 3. or
 """
 
+print("*"*25)
 bool_output = True or not False and False
+print("Order of Evaluation for 'True or not False and False'")
+print("not False = 'True'")
+print("True and False = 'False'")
+print("True or False = 'True'")
 #  True
-print(bool_output)
+print("Final evaluation for boolean 'True or not False and False' is = " + str(bool_output))
+
+print("*"*25)
 
 bool_output_1 = (10 == 10 or not 10 > 10) and 10 > 10
+print("showing order of precedence operation for boolean '(10 == 10 or not 10 > 10) and 10 > 10'")
+print("1. parentheses operation first")
+print("2. 10==10 = 'True'")
+print("3. not 10 > 10 = 'True'")
+print("4. 'True' or 'True' = 'True'")
+print("5. 10>10 = 'False'")
+print("'True' and 'False' = 'False'")
+print("The boolean evaluation for '(10 == 10 or not 10 > 10) and 10 > 10' is "+ str(bool_output_1))
+
 # True or True -> True and False -> False
 print(bool_output_1)
---
+++ PythonProj1/controlstructure/multiplelist.py
@@ -0,0 +1,8 @@
+"""
+Iterating multiple list at the same time
+"""
+
+l1 = [1,2,3]
+l2 = [6,7,8,20,30,40]
+
+for a, b in zip(l1,l2):
